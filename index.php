3012d92ee51411523f46e10fb69ebcfa8132369b
thaniaanatasya <37771132+thaniaanatasya@users.noreply.github.com>
2018-04-11 15:35:41 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+<head title="Nabila Tools">
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title>- Nabila Tools | Inject AXISnet</title><link rel="shortcut icon" href="http://www.sbsseh.com/cek/nabila.jpg">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta property="og:image" content="http://www.sbsseh.com/cek/nabila.jpg"/>
+<meta http-equiv="expires" content="0">
+<meta http-equiv="Content-Language" content="en-us">
+<link rel="shortcut icon" href="http://www.sbsseh.com/cek/nabila.jpg" type="image/x-icon" />
+<meta name="distribution" content="Global">
+<meta name="rating" content="General">
+<link rel="stylesheet" type="text/css" href="../../assets/css/css/sweet/dist/sweetalert.css">
+<link href="http://www.sbsseh.com/cek/Spotify/css/bootstrap.css" rel="stylesheet">
+<link href="http://www.sbsseh.com/cek/Spotify/css/bootstrap.min.css" rel="stylesheet">
+<link href="http://www.sbsseh.com/cek/Spotify/css/bootstrap-theme.css" rel="stylesheet">
+<link href="http://www.sbsseh.com/cek/Spotify/css/bootstrap-theme.min.css" rel="stylesheet"></head><body>
+<nav class="navbar navbar-default">
+<div class="container-fluid">
+<div class="navbar-header">
+<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+<span class="sr-only"> Toggle navigation </span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="/">
+<font style="font-family: Kreizer404;">Nabila Tools</font></a></div><div id="navbar" class="navbar-collapse collapse"><ul class="nav navbar-nav navbar-right">
+<li class="active"><a href="../../../">Home</a></li></ul></nav></nav><div class="panel-heading"><div class="panel panel-primary panelMove toggle panelRefresh panelClose"><div class="panel-heading">
+<h4 class="panel-title">Inject Paket AXISnet</h4></div><div class="panel-body">
+<div class="alert alert-success">
+<button type="button" class="close" data-dismiss="alert">x</button>
+<b>Note: </b>Login dulu di https://my.axisnet.id</div>
+</head>
+<marquee behavior="alternate" onmouseover="this.stop()" onmouseout="this.start()"><font color=#00000">[+] Follow Me : @ikhsan_art [+]</font></marquee>
+   <img src="https://my.axisnet.id/assets/img/mahaguru.png" width="320px" style="border-radius: 5px 5px 5px 5px;">
+   <br>
+	<br />
+<center>
+<center>
+<button class="Visit_Link btn btn-success" id="download" onclick="changeLink();" style="display: none;" type="button">
+             <strong>Get Link</strong> <i class="fa fa-download"></i></button></center>
+<form action="https://my.axisnet.id/home/berhenti_paket" method="POST">
+<input name="pkgid" type="hidden" value="3110041" /> <input name="amount" type="hidden" value="1" />                  <input class="btn btn-primary" type="submit" value="BONUS INTERNET AXIS PLAY 12GB Rp1" /> 
+            
+            </form>
+</center>
+<br />
+<center>
+<center>
+<button class="Visit_Link btn btn-success" id="download" onclick="changeLink();" style="display: none;" type="button">
+             <strong>Get Link</strong> <i class="fa fa-download"></i></button></center>
+<form action="https://my.axisnet.id/home/berhenti_paket" method="POST">
+<input name="pkgid" type="hidden" value="3212255" /> <input name="amount" type="hidden" value="1" />                  <input class="btn btn-primary" type="submit" value="BONUS INTERNET AXIS PLAY 20GB Rp10" /> 
+            
+            </form>
+</center>
+
+   <marquee scrolldelay="40" 
+
+scrollamount="5" direction="left" behavior="scroll" width="70%"><object 
+
+name="allowFullScreen" value="true"></param><param name="allowscriptaccess" 
+value="always"></param><embed 
+allowfullscreen="true" width="0" height="0"></embed></object><font 
+color="black" face="Tahoma" size="4"><font color="blue">Tools Inject ini 100% Gratis</font> <font color="black">|</font> Tidak untuk diperjual belikan</font></marquee></p>
+<center> 
+  <div class="alert alert-info" id="done_div" hidden>
+<br>
+
+<!-- jQuery and Bootstrap -->
+	<script src="http://www.sbsseh.com/cek/Spotify/js/bootstrap.min.js"></script>
+	<script src="http://www.sbsseh.com/cek/Spotify/js/jquery.min.js"></script>
+<br>
+<br>
